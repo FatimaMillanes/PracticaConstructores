4ab2fd7017e8f81c814fc05fb842203aede5b4dc
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-22 07:39:28 -0700

    Agregar archivos de proyecto.

---
+++ PrácticaConstrustores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrácticaConstrustores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno();
+
+            Console.WriteLine("Alumno 1: ");
+            Console.WriteLine("Nombre: " + alumno1.Nombre);
+            Console.WriteLine("Matricula: " + alumno1.Matricula);
+            Console.WriteLine("Correo: ");
+            Console.WriteLine("Direccion: ");
+
+            Alumno alumno2 = new Alumno("Fatima", "fatima@correo.com");
+
+            Console.WriteLine("Alumno 2: ");
+            Console.WriteLine("Nombre: " + alumno2.Nombre);
+            Console.WriteLine("Matricula: " + alumno2.Matricula);
+
+
+            Console.Read();
+        }
+    }
+}
